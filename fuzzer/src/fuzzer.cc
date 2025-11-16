25420328838600198d6df2fe95ce5fb468810140
Davis Muro <git@davisraym.com>
2025-11-15 21:09:20 -0800

    feat: add initial fuzzer binary

---
+++ fuzzer/src/fuzzer.cc
@@ -0,0 +1,8 @@
+/* Fuzzer Binary
+ *
+ * This program is responsible for fuzzing the TPM Reference
+ * https://github.com/TrustedComputingGroup/TPM source code. Specifically
+ * the `SendCommand` TPM API.
+ */
+
+int main(void) { return 0; }
