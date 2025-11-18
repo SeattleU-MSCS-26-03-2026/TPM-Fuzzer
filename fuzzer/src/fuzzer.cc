6478020f963b521a0704e2507b0bd2e92731cd95
tpranoto <26911742+tpranoto@users.noreply.github.com>
2025-11-17 20:05:14 -0800

    create harness for TPM send command

--- fuzzer/src/fuzzer.cc
+++ fuzzer/src/fuzzer.cc
@@ -5,6 +5,27 @@
  * https://github.com/TrustedComputingGroup/TPM source code. Specifically
  * the `SendCommand` TPM API.
  */
-#include <harness/tpm_wrapper.h>
+// #include <harness/tpm_wrapper.h>
 
-int main(void) { return 0; }
+#include <cstddef>
+
+extern "C" {
+#include <harness/tpm_wrapper.h>
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
+  unsigned char locality = 0;
+
+  _IN_BUFFER request;
+  request.Buffer = (unsigned char*)Data;
+  request.BufferSize = (unsigned int)Size;
+
+  _OUTPUT_BUFFER responseBuf[4096];
+  _OUT_BUFFER response;
+  response.Buffer = *responseBuf;
+  response.BufferSize = 4096;
+
+  TPMSendCommand(locality, request, &response);
+
+  return 0;
+}
