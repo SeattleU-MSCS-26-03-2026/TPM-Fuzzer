24916f5e38acf724be53c0c9887105f05177b53f
tpranoto <26911742+tpranoto@users.noreply.github.com>
2025-11-21 15:09:16 -0800

    fix: fix the start fuzzer script

--- scripts/start-fuzzer.sh
+++ scripts/start-fuzzer.sh
@@ -1,20 +1,16 @@
 #!/bin/env bash
 set -e
 
-generate_coverage() {
-    echo "Creating coverage report..."
-    llvm-profdata merge -sparse /srv/build/Fuzzer.profraw -o /srv/build/Fuzzer.profdata
-
-    llvm-cov show /srv/build/Fuzzer \
-        -instr-profile=/srv/build/Fuzzer.profdata \
-        -format=html \
-        -output-dir=/srv/build/coverage \
-        $(find /srv/fuzzer/src /srv/fuzzer/vendor/TPM -type f \( -name '*.c' -o -name '*.cc' \))
-    
-    echo "Done!"
-}
-
-trap generate_coverage EXIT
-
 echo "Running fuzzer..."
-./build/Fuzzer /srv/corpus
+./build/Fuzzer -runs=100000 /srv/corpus
+
+echo "Creating coverage report..."
+llvm-profdata merge -sparse /srv/build/Fuzzer.profraw -o /srv/build/Fuzzer.profdata
+
+llvm-cov show /srv/build/Fuzzer \
+    -instr-profile=/srv/build/Fuzzer.profdata \
+    -format=html \
+    -output-dir=/srv/build/coverage \
+    $(find /srv/fuzzer/src /srv/fuzzer/vendor/TPM -type f \( -name '*.c' -o -name '*.cc' \))
+
+echo "Done!"
