b5f3389611e102bdded10c80464e6b77b530e092
Davis Raymond Muro <git@davisraym.com>
2026-01-20 18:31:08 -0800

    feat: send TPM2_StartUp command before requests

--- fuzzer/src/fuzzer.cc
+++ fuzzer/src/fuzzer.cc
@@ -19,6 +19,15 @@ extern "C" {
 
 const size_t kMaxBuffers = 1048576;
 const int kDefaultLocality = 0;
+const uint8_t kTPM2Startup[] = {
+    // TPM2_ST_NO_SESSIONS header
+    0x80, 0x01,              // tag: TPM_ST_NO_SESSIONS (0x8001)
+    0x00, 0x00, 0x00, 0x0C,  // commandSize: 12 bytes (0x0000000C)
+    0x00, 0x00, 0x01, 0x44,  // commandCode: TPM_CC_Startup (0x00000144)
+
+    // Parameters
+    0x00, 0x00  // startupType: TPM_SU_CLEAR (0x0000)
+};
 
 // Called for each fuzzing input
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
@@ -29,6 +38,20 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
 
   TPMManufactureIfNeeded();
   TPMStartup();
+
+  // Send out TPM2_Startup
+  {
+    struct InBuffer startUpRequest;
+    startUpRequest.buffer = kTPM2Startup;
+    startUpRequest.buffer_size = sizeof(kTPM2Startup);
+
+    char OutputBuffer[kMaxBuffers];
+    struct OutBuffer response;
+    response.buffer = (uint8_t*)OutputBuffer;
+    response.buffer_size = kMaxBuffers;
+
+    TPMSendCommand(kDefaultLocality, startUpRequest, &response);
+  }
 
   for (auto& cmd : commands) {
     struct InBuffer request;
